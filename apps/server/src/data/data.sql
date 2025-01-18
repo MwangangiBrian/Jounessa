14079490751a3f109f089e58dd0d51591b6e838a
MwangangiBrian <bmwangimwangangi@gmail.com>
2025-01-18 21:48:21 +0300

    chore: database setup and controllers

--- apps/server/src/data/data.sql
+++ apps/server/src/data/data.sql
@@ -5,3 +5,22 @@ CREATE TABLE IF NOT EXISTS users(
     password VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 )
+
+CREATE TABLE IF NOT EXISTS Destinations (
+    destination_id SERIAL PRIMARY KEY,
+    destination_name VARCHAR(100) NOT NULL,
+    country VARCHAR(50),
+    city VARCHAR(50),
+    description TEXT,
+    average_cost DECIMAL(10, 2)
+);
+
+CREATE TABLE IF NOT EXISTS Hotels (
+    hotel_id SERIAL PRIMARY KEY,
+    hotel_name VARCHAR(100) NOT NULL,
+    destination_id INT REFERENCES Destinations(destination_id) ON DELETE CASCADE,
+    address TEXT,
+    contact_number VARCHAR(15),
+    price_per_night DECIMAL(10, 2),
+    rating DECIMAL(2, 1) CHECK (rating BETWEEN 0 AND 5)
+);
--- apps/server/src/models/userModel.ts
+++ apps/server/src/models/userModel.ts
@@ -6,18 +6,21 @@ export const getAllUsersService = async () => {
 };
 
 export const getUserByIdService = async (id: any) => {
-  const result = await pool.query('SELECT * FROM users WHERE id= $1', [id]);
+  const result = await pool.query('SELECT * FROM users WHERE user_id= $1', [
+    id,
+  ]);
   return result.rows[0];
 };
 
 export const createUserService = async (
+  firstName: string,
+  lastName: string,
   email: string,
-  name: string,
   password: string
 ) => {
   const result = await pool.query(
-    'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING name',
-    [name, email, password]
+    'INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING name',
+    [firstName, lastName, email, password]
   );
   return result.rows[0];
 };
@@ -28,7 +31,7 @@ export const updateUserService = async (
   email: string
 ) => {
   const result = await pool.query(
-    'UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *',
+    'UPDATE users SET name=$1, email=$2 WHERE user_id=$3 RETURNING *',
     [name, email, id]
   );
   return result.rows[0];
@@ -36,7 +39,7 @@ export const updateUserService = async (
 
 export const deleteUserService = async (id: any) => {
   const result = await pool.query(
-    'DELETE FROM users WHERE id=$id RETURNING *',
+    'DELETE FROM users WHERE user_id=$id RETURNING *',
     [id]
   );
   return result;
