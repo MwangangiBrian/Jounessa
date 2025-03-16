392f31694f64553ded2dbc6a22d4401847e9135c
MwangangiBrian <bmwangimwangangi@gmail.com>
2025-03-16 20:58:40 +0300

    feat: add Sidebar component with tabs definition

---
+++ apps/client/src/components/admin-dashboard/Sidebar.tsx
@@ -0,0 +1,20 @@
+import { DashboardIcon } from "@radix-ui/react-icons"
+
+interface Tabs {
+    name: string;
+    icon: any;
+}
+
+const tabs: Tabs = [
+    {
+        name: "Overview",
+        icon: DashboardIcon
+    }
+]
+
+export const Sidebar = () => {
+  return (
+    <>
+    </>
+  )
+}
